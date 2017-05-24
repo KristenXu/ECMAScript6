499dfd8e9c67160fcddab44661dab980b8827bec
Chainn <578579786@qq.com>
2017-05-24 22:32:26 +0800

    Add proxy get extend

--- es6/proxyReflect/proxyGet.js
+++ es6/proxyReflect/proxyGet.js
@@ -13,4 +13,15 @@ var proxy = new Proxy(person, {
 });
 
 proxy.name // "张三"
-proxy.age // 抛出一个错误
+// proxy.age // 抛出一个错误
+
+let proto = new Proxy({name: '123'}, {
+  get(target, propertyKey, receiver) {
+    console.log('GET '+propertyKey);
+    return target[propertyKey];
+  }
+});
+
+let obj = Object.create(proto);
+console.log(obj.ww) // "undefiend"
+console.log(obj.name) // "123"
