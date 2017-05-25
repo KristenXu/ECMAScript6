85cd7557f8b723726062ec999768d0132bb1841c
Chainn <578579786@qq.com>
2017-05-25 22:32:27 +0800

    Add chain

--- es6/proxyReflect/proxyGet.js
+++ es6/proxyReflect/proxyGet.js
@@ -25,3 +25,33 @@ let proto = new Proxy({name: '123'}, {
 let obj = Object.create(proto);
 console.log(obj.ww) // "undefiend"
 console.log(obj.name) // "123"
+
+//链式操作
+var allFun = {
+  double : n => n * 2,
+
+  pow : n => n * n,
+
+  reverseInt : n => n.toString().split("").reverse().join("") | 0
+}
+var pipe = (function () {
+  return function (value) {
+    var funcStack = [];
+    var oproxy = new Proxy({} , {
+      get : function (pipeObject, fnName) {
+        if (fnName === 'get') {
+          return funcStack.reduce(function (val, fn) {
+            return fn(val);
+          },value);
+        }
+        funcStack.push(allFun[fnName]);
+        return oproxy;
+      }
+    });
+
+    return oproxy;
+  }
+}());
+
+
+console.log(pipe(3).double.pow.reverseInt.get); // 63
