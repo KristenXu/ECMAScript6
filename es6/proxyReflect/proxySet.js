e7becb3539205e6b80639d98bc3cbfe765ce0549
Chainn <578579786@qq.com>
2017-05-25 22:39:47 +0800

    Add internal property check handler

--- es6/proxyReflect/proxySet.js
+++ es6/proxyReflect/proxySet.js
@@ -21,3 +21,26 @@ person.age = 100;
 console.log(person.age) // 100
 // person.age = 'young' // 报错
 // person.age = 300 // 报错
+
+var handler = {
+  get (target, key) {
+    invariant(key, 'get');
+    return target[key];
+  },
+  set (target, key, value) {
+    invariant(key, 'set');
+    target[key] = value;
+    return true;
+  }
+};
+function invariant (key, action) {
+  if (key[0] === '_') {
+    throw new Error(`Invalid attempt to ${action} private "${key}" property`);
+  }
+}
+var target = {};
+var proxy = new Proxy(target, handler);
+proxy._prop
+// Error: Invalid attempt to get private "_prop" property
+proxy._prop = 'c'
+// Error: Invalid attempt to set private "_prop" property
