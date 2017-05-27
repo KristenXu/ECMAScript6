591dcd8ff145e90d34b37fb8f1d44722900c11b0
Chainn <578579786@qq.com>
2017-05-27 22:55:54 +0800

    Add proxy set

---
+++ es6/proxyReflect/reflectSet.js
@@ -0,0 +1,14 @@
+var myObject = {
+    foo: 4,
+    set bar(value) {
+        return this.foo = value;
+    },
+};
+
+var myReceiverObject = {
+    foo: 0,
+};
+
+Reflect.set(myObject, 'bar', 1, myReceiverObject);
+myObject.foo // 4
+myReceiverObject.foo // 1
